a89591a99943c77d9b1542d5dd9aab32fe191fda
supadata <2655202035@qq.com>
2018-01-15 16:26:20 +0800

    提交tabbar

--- WYMusic/ViewController.swift
+++ WYMusic/ViewController.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var cotainnerView: UIView!
+    @IBOutlet var tabbarView: UIView!
+    
+    @IBOutlet var discoverBtn: UIButton!
+    @IBOutlet var discoverImg: UIImageView!
+    @IBOutlet var discoverLab: UILabel!
+    
+    @IBOutlet var mineBtn: UIButton!
+    @IBOutlet var mineImg: UIImageView!
+    @IBOutlet var mineLab: UILabel!
+    
+    @IBOutlet var friendsBtn: UIButton!
+    @IBOutlet var friendsImg: UIImageView!
+    @IBOutlet var friendsLab: UILabel!
+    
+    @IBOutlet var acountBtn: UIView!
+    @IBOutlet var acountIMg: UIImageView!
+    @IBOutlet var acountLab: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
